bc6d7de0de6ac59e2d2226c4bc612c402e2f2397
standbaqui <ysc89@naver.com>
2020-11-18 10:19:46 +0900

    V1

---
+++ huminity.ino
@@ -0,0 +1,44 @@
+#include <DHT.h>
+#define DHTPIN 7
+
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+    dht.begin();
+    Serial.begin(115200);
+    Serial.println("Start DHT");
+}
+void loop() {
+  delay(2000);
+
+  float h = dht.readHumidity();// 습도를 측정합니다.
+  float t = dht.readTemperature();// 온도를 측정합니다.
+  float f = dht.readTemperature(true);// 화씨 온도를 측정합니다.
+
+  // 값 읽기에 오류가 있으면 오류를 출력합니다.
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // 보정된 화씨 값을 가져옵니다.
+  float hif = dht.computeHeatIndex(f, h);
+  // 보정된 섭씨 값을 가져옵니다.
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+} 
